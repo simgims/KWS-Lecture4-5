763c13c03828559ad1e668e72e20c6b77e153f8d
Simen Gimsøy <simgims@hotmail.com>
2025-02-04 12:40:10 +0100

    Changed formating

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello React</h1>);
+createRoot(document.getElementById("root")!).render(<h1>Hello React!</h1>);
