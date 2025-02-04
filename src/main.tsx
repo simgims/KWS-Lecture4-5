644435daa0fd1b6d3be8155e058ac87e7834820b
Simen Gimsøy <simgims@hotmail.com>
2025-02-04 12:56:25 +0100

    Updates to ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello React!</h1>);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(<h1>Hello React!</h1>);
+}
